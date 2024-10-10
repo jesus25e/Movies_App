ec5959995ac4daacd85562d5b8fdbbdb70caabf3
jesus25e <prietojesus226@gmail.com>
2024-10-10 16:21:26 -0500

    EnRutar

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import {dirname,join} from 'path'
+import { fileURLToPath } from 'url'
+import indexRouter from './router/index.js'
+
+const app = express()
+//app.get('/',(req,res)=>res.send('index'))
+
+//obtener Url de la carpeta :D
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(join(__dirname , 'views'))
+app.set('views', join(__dirname,'views'))
+app.use(indexRouter)
+
+//D:\UNI\Curso_JavaScript\Movies_App\src\views
+app.set('view engine','ejs')
+
+app.listen(3000)
+console.log("El servidor esta siendo escuchado en el puerto ",3000)
