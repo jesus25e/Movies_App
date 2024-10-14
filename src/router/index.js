416cf91f7905c8582a106e031e59f806cdd2d898
jesus25e <prietojesus226@gmail.com>
2024-10-13 19:52:31 -0500

    navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
 const router = Router()
 
-router.get('/' , (req,res) => res.render('index'))
-router.get('/about' , (req,res) => res.render('about')),
-router.get('/contact' , (req,res) => res.render('contact'))
+router.get('/' , (req,res) => res.render('index',{title:'Index'}))
+router.get('/about' , (req,res) => res.render('about',{title: 'Contactanos'})),
+router.get('/contact' , (req,res) => res.render('contact',{title: 'Sobre Nosotros'}))
 
 export default router
