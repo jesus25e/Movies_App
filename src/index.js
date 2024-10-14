71a9914d304fd7a46b21387af173443507d4b572
jesus25e <prietojesus226@gmail.com>
2024-10-14 01:04:40 -0500

    conexion

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import {dirname,join} from 'path'
 import { fileURLToPath } from 'url'
 import indexRouter from './router/index.js'
-
+import { Conectar } from './public/services/conexion.js'
 const app = express()
 //app.get('/',(req,res)=>res.send('index'))
 
@@ -14,6 +14,8 @@ app.use(indexRouter)
 
 //D:\UNI\Curso_JavaScript\Movies_App\src\views
 app.set('view engine','ejs')
+
+Conectar()
 
 app.listen(3000)
 console.log("El servidor esta siendo escuchado en el puerto ",3000)
